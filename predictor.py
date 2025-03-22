715f8e612e2f19ff7e5572a45d0093759944fc5a
JadaNselee <nselejadaa@gmail.com>
2025-03-22 01:03:50 -0400

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,1 +1,4 @@
-# financial-forecasting-tool-tracker
+print("Welcome to the Investment Return Predictor!")
+
+# Ask user for investment details
+initial_amount = float(input("Enter the amount you're investing (in $): "))
